d0ede7410037182ce0bb56e6b8cab282ca7f596d
Franz Martinez <fmmartin@ualberta.ca>
2014-07-16 17:33:32 -0600

    submitsim.py functional

--- submitsim.py
+++ submitsim.py
@@ -3,26 +3,47 @@ import os
 import shutil
 from sys import argv
 
-nproc = 10
+#argument provided is the name of the folder, which in turn has all the
+#information needed for the script
+if len(argv) == 1:
+	print "you must provide name of simulation (folder name) as an argument"
+	quit()
+
+dataname = argv[1]
+
+nproc = int(dataname[8:11])
 
 dirs = []
 
-l = []
-
-if (nproc > 9):
-	for i in range(0,10):
-		name = 'map-0' + str(i)
-		dirs.append(name)
-	for i in range(10,nproc):
-		name = 'map-'  + str(i)
-		dirs.append(name)
+if nproc > 9:
+	if nproc > 99:
+		if nproc > 999:
+			print "error: too many processsors will be used"
+		else:
+			for i in range(0,10):
+				name = 'part-00' + str(i)
+				dirs.append(name)
+			for i in range(10,100):
+				name = 'part-0'  + str(i)
+				dirs.append(name)
+			for i in range(100,nproc):
+				name = 'part-'  + str(i)
+				dirs.append(name)
+	else:
+		for i in range(0,10):
+			name = 'part-00' + str(i)
+			dirs.append(name)
+		for i in range(10,nproc):
+			name = 'part-0'  + str(i)
+			dirs.append(name)
 else:
 	for i in range(0,nproc):
-		name = 'map-'  + str(i)
+		name = 'part-00'  + str(i)
 		dirs.append(name)
 
 currentpath = os.getcwd()
 for i in range(0,nproc):
-	workingd = currentpath + '/' + dirs[i]
+	workingd = currentpath + '/' + dataname + '/'  + dirs[i]
+	#print workingd
 	os.chdir(workingd)
 	os.system('qsub submit_cluster.pbs')
