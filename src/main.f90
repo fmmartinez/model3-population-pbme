75275da99facb420ded18ba1e29688d97da3de05
Franz Martinez <fmmartin@ualberta.ca>
2015-05-27 18:42:50 -0600

    removed calls to random number generators not used

--- src/main.f90
+++ src/main.f90
@@ -231,8 +231,6 @@ contains
 subroutine iniconc()
 implicit none
 
-!integer :: i
-
 open (666,file='md.in')
 read(666,*)
 read(666,*) np,delta,nosc,ome_max
@@ -247,13 +245,6 @@ read(666,*) bath,init
 read(666,*)
 read(666,*) ng,nb,nd
 close(666)
-
-!call random_seed(size=seed_dimension)
-!allocate (seed(seed_dimension))
-!do i=1,seed_dimension
-!  seed(i) = 3*2**i-1
-!enddo
-!call random_seed(put=seed)
 
 end subroutine iniconc
 
